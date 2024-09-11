3b74eb01f28684726da4944a9244b0971d8f85a3
Shivaraja2000 <shivarajammrinfotech@gmail.com>
2024-09-12 00:15:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+    document.getElementById('preloader').style.display = 'none';
+    document.querySelector('.index-content').classList.add('loaded');
+});
